1f1a701c548343abad211115276f437440ab7eee
AegisVP <vlad@pysarenko.com>
2022-08-11 19:13:18 +0200

    Original setup

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,5 @@
+import { theme } from '../Common/Theme';
+
 export const App = () => {
   return (
     <div
@@ -10,7 +12,8 @@ export const App = () => {
         color: '#010101'
       }}
     >
-      React homework template
+      React homework template<br/>
+        {theme.mp(1, 2, 3, 'auto')}
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { ThemeProvider } from 'styled-components';
+import { App } from 'components/App/App';
+import { theme } from 'components/Common/Theme.jsx';
 import './index.css';
 
 ///////////////////
@@ -12,7 +14,7 @@ import './index.css';
 // передав в пропе basename точное название твоего репозитория.
 // Слеши в начале и конце строки обязательны.
 //
-// <BrowserRouter basename="/_____repo_name______/">
+// <BrowserRouter basename="/goit-react-hw-03-image-finder/">
 //   <App />
 // </BrowserRouter>
 //
@@ -20,6 +22,8 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
